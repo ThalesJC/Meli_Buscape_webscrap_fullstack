4e9622bcac801093d36a44457cf58c024d8daaea
ThalesJC <thales.soul96@gmail.com>
2023-04-12 21:57:03 -0300

    Feat: Card para exposição dos produtos

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,12 @@
+import { Card } from "./Components/Card"
 import {Heading} from "./Components/Heading"
 
 function App() {
   return (
-    <Heading />
+    <>
+      <Heading />
+      <Card />
+    </>
   )
 }
 
---
+++ front-end/src/Components/Card.tsx
@@ -0,0 +1,13 @@
+export function Card() {
+    return (
+        <div>
+            <img src="https://via.placeholder.com/150" alt="Product" />
+            <section>
+                <h1>Nome do Produto</h1>
+                <p>Descrição do produto</p>
+                <p>Preço</p>
+                <button type="button">Go to web</button>
+            </section>
+        </div>
+    )
+}
